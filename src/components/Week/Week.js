c0e0ad729b9701199a952ddb1787ced9804bfc5e
lutsenkoanv <theri3008@gmail.com>
2020-02-23 00:15:52 +0200

    new fix of week

--- src/components/Week/Week.js
+++ src/components/Week/Week.js
@@ -1,13 +1,13 @@
 import React from 'react';
+import Proptypes from 'prop-types';
 import 'bootswatch/dist/lux/bootstrap.min.css';
-import { daysTitle, monthes } from '../../utils/constants';
+import { daysTitle, months } from '../../utils/constants';
 import { Button } from '../Button/Button';
 
 import './Week.css';
 
 const arrWeek = [...Array(7).keys()];
 const arrHours = [...Array(24).keys()];
-const initialDate = new Date();
 
 class Week extends React.Component {
     state = {
@@ -17,13 +17,14 @@ class Week extends React.Component {
     };
 
     componentDidMount() {
-      this.getWeek(initialDate);
+      this.getWeek(this.props.date);
     }
 
-    getWeek = (initial) => {
+    getWeek = (param) => {
       const month = [];
       const date = [];
       const years = [];
+      const initial = new Date(param.getTime());
 
       for (let i = 1; i <= 7; i += 1) {
         const first = initial.getDate() - initial.getDay() + i;
@@ -48,7 +49,7 @@ class Week extends React.Component {
       return (
         <>
           <div className="date__heading">
-            {`${date[0]} - ${date[6]} ${monthes[6]} ${years[6]}`}
+            {`${date[0]} - ${date[6]} ${months[+month[6]]} ${years[6]}`}
           </div>
           <table className="table table-hover">
             <thead>
@@ -90,5 +91,9 @@ class Week extends React.Component {
       );
     }
 }
+
+Week.propTypes = {
+  date: Proptypes.instanceOf(Date).isRequired,
+};
 
 export default Week;
