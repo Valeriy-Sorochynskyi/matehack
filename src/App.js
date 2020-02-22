bc57ac84dfd24edd0ede6fe75b06c987ebbea905
Valeriy-Sorochynskyi <haribol162@gmail.com>
2020-02-22 11:02:25 +0200

    reseted all

--- src/App.js
+++ src/App.js
@@ -1,28 +1,9 @@
 import React from 'react';
 import './App.css';
 
-import { Tabs } from './components/Tabs/Tabs';
-
-// eslint-disable-next-line no-unused-vars
-const tabs = [
-  {
-    title: 'Tab 1',
-    content: 'Some text 1',
-  },
-  {
-    title: 'Tab 2',
-    content: 'Some text 2',
-  },
-  {
-    title: 'Tab 3',
-    content: 'Some text 3',
-  },
-];
-
 const App = () => (
   <>
-    <h1>React tabs</h1>
-    <Tabs tabs={tabs} />
+    <h1>Hello hackaton!!!</h1>
   </>
 
 );
