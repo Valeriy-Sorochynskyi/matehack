7b06726efd4d0d6585517d9ba4449a6a73259f51
Valeriy-Sorochynskyi <haribol162@gmail.com>
2020-02-22 13:07:41 +0200

    structure

---
+++ src/components/Content/Content.js
@@ -0,0 +1,12 @@
+import React from 'react';
+// import Proptypes from 'prop-types';
+
+export class Content extends React.Component {
+  state = {}
+
+  render() {
+    return (
+      <h1>It is Content</h1>
+    );
+  }
+}
---
+++ src/components/Header/Header.js
@@ -0,0 +1,15 @@
+import React from 'react';
+// import Proptypes from 'prop-types';
+
+export class Header extends React.Component {
+  state = {};
+
+  render() {
+    return (
+      <header>
+        {/* <Pagination /> */}
+        {/* <Tabs /> */}
+      </header>
+    );
+  }
+}
