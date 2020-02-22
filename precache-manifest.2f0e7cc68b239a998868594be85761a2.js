c4294fffd8168589ae5e191eeab7042b348f4a97
Valeriy-Sorochynskyi <haribol162@gmail.com>
2020-02-22 11:10:45 +0200

    Updates

---
+++ precache-manifest.2f0e7cc68b239a998868594be85761a2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1c67c399179db63bd535540444b5a1ed",
+    "url": "./index.html"
+  },
+  {
+    "revision": "0cee37cf718d15d7e20e",
+    "url": "./static/css/main.8b0cfe4b.chunk.css"
+  },
+  {
+    "revision": "e883103dc38b42daf82f",
+    "url": "./static/js/2.a80e6b24.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "./static/js/2.a80e6b24.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0cee37cf718d15d7e20e",
+    "url": "./static/js/main.f6822b1b.chunk.js"
+  },
+  {
+    "revision": "a17aa78490837737dc51",
+    "url": "./static/js/runtime-main.6991981f.js"
+  }
+]);
---
+++ static/js/main.f6822b1b.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonpmatehack=this.webpackJsonpmatehack||[]).push([[0],{3:function(e,n,t){e.exports=t(9)},8:function(e,n,t){},9:function(e,n,t){"use strict";t.r(n);var a=t(0),c=t.n(a),r=t(2),l=t.n(r),o=(t(8),function(){return c.a.createElement(c.a.Fragment,null,c.a.createElement("h1",null,"Hello hackaton!!!"))});l.a.render(c.a.createElement(o,null),document.getElementById("root"))}},[[3,1,2]]]);
+//# sourceMappingURL=main.f6822b1b.chunk.js.map
