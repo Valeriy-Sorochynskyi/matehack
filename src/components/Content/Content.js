aa00365aa6851d54916d99f094676ad4ec486bca
Valeriy-Sorochynskyi <haribol162@gmail.com>
2020-02-22 20:16:26 +0200

    render tables

--- src/components/Content/Content.js
+++ src/components/Content/Content.js
@@ -1,5 +1,5 @@
 import React from 'react';
-// import Proptypes from 'prop-types';
+import Proptypes from 'prop-types';
 import Month from '../Month/Month';
 import { Day } from '../Day/Day';
 import Week from '../Week/Week';
@@ -8,12 +8,18 @@ export class Content extends React.Component {
   state = {}
 
   render() {
+    const { content } = this.props;
+
     return (
       <div className="container">
-        <Month />
-        <Day />
-        <Week />
+        {content === 'day' && <Day />}
+        {content === 'month' && <Month />}
+        {content === 'week' && <Week />}
       </div>
     );
   }
 }
+
+Content.propTypes = {
+  content: Proptypes.string.isRequired,
+};
